06297aa2ddd6ddc56404ec3e3eabc008da45a50f
theashu02 <ashutoshchauhan1919@gmail.com>
2024-12-17 22:00:40 +0530

    first commit

---
+++ frontend/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+// import type {PropsWithChildren} from 'react';
+import { Text } from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <Text>
+      hello this is react application
+    </Text>
+  );
+}
+
+export default App;
